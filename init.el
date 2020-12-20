02d619f9adfd155c402bb680e6861bad0a18cbd1
Jay Barra <jay.barra@nasa.gov>
2020-12-20 07:44:31 -0500

    using doom themes

--- init.el
+++ init.el
@@ -18,7 +18,13 @@
 ;; Install Fira Code to the system first
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 140)
 
-(load-theme 'tango-dark)
+;;(load-theme 'tango-dark)
+(use-package doom-themes
+  :init (load-theme 'doom-one t))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -69,11 +75,12 @@
   :defer 1
   :after counsel)
 
-(use-package simple-modeline
-  :hook (after-init . simple-modeline-mode))
+;; (use-package simple-modeline
+;;   :hook (after-init . simple-modeline-mode))
 
 (use-package paredit
-  :hook (prog-mode . paredit-mode))
+  :hook ((cider-mode . paredit-mode)
+	 (prog-mode . paredit-mode)))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -108,33 +115,55 @@
 (use-package git-timemachine)
 (use-package git-messenger)
 (use-package git-gutter
+  :diminish
   :hook (after-init . global-git-gutter-mode))
 
 (use-package fullframe)
 
 (use-package clojure-mode)
 (use-package cljsbuild-mode)
-(use-package elein)
 
-(use-package cider)
+(use-package cider
+  :ensure t
+  :defer t
+  :config
+  (setq cider-repl-history-file ".cider-repl-history")
+  (flycheck-clojure-setup))
 
 (use-package flycheck-clojure
-  :init (flycheck-clojure-setup))
+  :defer t
+  :commands (flycheck-clojure-setup)
+  :config
+  (eval-after-load 'flycheck
+    '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+(setq-default flycheck-emacs-lisp-load-path 'inherit)
+
+(use-package flycheck-pos-tip
+  :ensure t
+  :after flycheck)
+
+(use-package flycheck-color-mode-line
+  :after flycheck)
 
 (use-package ruby-mode)
 (use-package ruby-hash-syntax)
 (use-package rspec-mode)
 
+(use-package python-mode
+  :ensure nil
+  :custom
+  (python-shell-interpreter "python3"))
+
 (use-package dockerfile-mode)
 (use-package docker-compose-mode)
 
-(use-package python-mode)
-
-(require 'init-osx-keys)
 (require 'init-exec-path)
 (require 'init-behaviors)
-(require 'init-flycheck)
-;;(require 'init-uniquify)
 (require 'init-projectile)
 
 (use-package uniquify-files)
@@ -157,13 +186,71 @@
 
 (use-package rg)
 
+(defun ds/org-font-setup ()
+  "Setup fonts for 'org-mode'."
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for headings
+  (dolist (face '((org-level-1 . 1.2)
+		  (org-level-2 . 1.1)
+		  (org-level-3 . 1.05)
+		  (org-level-4 . 1.0)
+		  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+		  (org-level-7 . 1.1)
+		  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Fix fixed-pitch in Org
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch))
+
+(defun ds/org-mode-setup()
+  "Setup 'org-mode'."
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org
+  :pin org
+  :hook (org-mode . ds/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-agenda-files
+	'(~/workspace/organization/todo.org))
+
+  (ds/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package markdown-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rg whitespace-cleanup-mode git-messenger fullframe git-timemachine gitignore-mode python-mode uniquify-files exec-path-from-shell syntax-subword flycheck-clojure eldoc-mode subword-mode cider magit flycheck-color-mode-line flycheck counsel-projectile clojure-mode hydra projectile helpful ivy-rich which-key simple-modeline rainbow-delimiters paredit flx smex counsel ivy diminish command-log-mode use-package)))
+   '(markdown-mode org-bullets doom-modeline doom-themes flycheck-pos-tip whitespace-cleanup-mode which-key use-package uniquify-files smex simple-modeline ruby-hash-syntax rspec-mode rg rainbow-delimiters python-mode paredit magit ivy-rich hydra helpful gitignore-mode git-timemachine git-messenger git-gutter fullframe flycheck-color-mode-line flycheck-clojure flx exec-path-from-shell elein dockerfile-mode docker-compose-mode diminish counsel-projectile company cljsbuild-mode beacon all-the-icons ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,11 +1,7 @@
 ;;; init-flycheck.el --- base flycheck configs
 ;;; Commentary:
 ;;; Code:
-(use-package flycheck
-  :hook (prog-mode . flycheck-mode))
 
-(use-package flycheck-color-mode-line
-  :after flycheck)
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
