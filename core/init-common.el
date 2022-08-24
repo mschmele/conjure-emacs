361bde41d586d3fc899fba22c84f412d2ea8638d
Jay Barra <jbarra@Jays-MacBook-Pro.local>
2022-08-24 08:31:49 -0400

    very nice JDK switching util found

--- core/init-common.el
+++ core/init-common.el
@@ -10,7 +10,7 @@
                          (last tokens)) "/")))
 
 (defun conjure-pretty-print-xml-region (begin end)
-  "Pretty-print XML in a sensible manner."
+  "Pretty-print XML in region betwen BEGIN and END in a sensible manner."
   (interactive "r")
   (save-excursion
     (nxml-mode)
@@ -21,9 +21,59 @@
 
 (require 'ansi-color)
 (defun display-ansi-colors ()
-  "Colorize buffer containining ANSI color strings. "
+  "Colorize buffer containining ANSI color strings."
   (interactive)
   (ansi-color-apply-on-region (point-min) (point-max)))
+
+(when *is-a-mac*
+  (setq JAVA_BASE "/Library/Java/JavaVirtualMachines"))
+
+(when *is-linux*
+  (message "[conjure] Linux distro java switching not implemented yet"))
+
+(defun switch-java--versions ()
+  "Return a list of installed JDK."
+  (seq-remove
+   (lambda (a) (or (equal a "." (equal a ".."))))
+   (directory-files JAVA_BASE)))
+
+(defun switch-java--save-enf ()
+  "Store original PATH and JAVA_HOME."
+  (when (not (boundp 'SW_JAVA_PATH))
+    (setq SW_JAVA_PATH (getenv "PATH")))
+  (when (not (boundp 'SW_JAVA_HOME))
+    (setq SW_JAVA_HOME (getenv "JAVA_HOME"))))
+
+(defun switch-java ()
+  "List installed JDKs and switch to one."
+  (interactive)
+  (switch-java--save-env)
+
+  (let ((ver (completing-read "Which Java: " (seq-map-indexed (lambda (e i) (list e i)) (switch-java--versions))
+                              nil t "")))
+    ;; switch java version
+    (setenv "JAVA_HOME" (concat JAVA_BASE "/" ver "/Contents/Home"))
+    (setenv "PATH" (concat (concat (getenv "JAVA_HOME") "/bin/java")
+                           ":" SW_JAVA_PATH)))
+  ;; show the current version
+  (switch-java-which-version?))
+
+(defun switch-java-default ()
+  "Restore default Java version."
+  (interactive)
+  (switch-java--save-env)
+
+  ;; switch java version
+  (setenv "JAVA_HOME" SW_JAVA_HOME)
+  (setenv "PATH" SW_JAVA_PATH)
+
+  ;; show version
+  (switch-java-which-version?))
+
+(defun switch-java-which-version? ()
+  "Display the current Java version."
+  (interactive)
+  (message (concat "Java HOME: " (getenv "JAVA_HOME"))))
 
 (provide 'init-common)
 ;;; init-common.el ends here
