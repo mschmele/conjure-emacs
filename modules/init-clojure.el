f3e30d150d7b9ebac8e634f2c49abb8e45552bb2
Mark Schmele <mschmele@gmail.com>
2022-12-19 07:15:02 -0700

    add portal functions

--- modules/init-clojure.el
+++ modules/init-clojure.el
@@ -59,5 +59,26 @@
   (let-routes 1)
   (context 2))
 
+;; Leverage an existing cider nrepl connection to evaluate portal.api functions
+;; and map them to convenient key bindings.
+
+;; def portal to the dev namespace to allow dereferencing via @dev/portal
+(defun portal-open ()
+  (interactive)
+  (cider-nrepl-sync-request:eval
+    "(do (ns dev) (def portal ((requiring-resolve 'portal.api/open))) (add-tap (requiring-resolve 'portal.api/submit)))"))
+
+(defun portal-clear ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/clear)"))
+
+(defun portal-close ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/close)"))
+
+;; NOTE: You do need to have portal on the class path and the easiest way I know
+;; how is via a clj user or project alias.
+(setq cider-clojure-cli-global-options "-A:portal")
+
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
